d27ffcd5dadced5ffd654c05c02cbfec1e22c04f
1015345966@qq.com <1015345966@qq.com>
2016-12-09 16:03:19 +0800

    frist

---
+++ 五子棋/PieceObj.swift
@@ -0,0 +1,17 @@
+//
+//  PieceObj.swift
+//  五子棋
+//
+//  Created by EDF on 16/12/7.
+//  Copyright © 2016年 EDF. All rights reserved.
+//
+
+import Foundation
+class PieceObj: NSObject {
+
+    var x: Int = 0
+    var y: Int = 0
+    
+    
+    
+}
